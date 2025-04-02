f9b3bc4edd0487816a5c853deddcc3a3169beabd
“nidhiisinghh” <nidhiisinghh2007@gmail.com>
2025-04-02 11:40:38 +0530

    Updated UI pages

---
+++ app/src/main/java/com/example/bookmydoc/MedicalActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bookmydoc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MedicalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_medical)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
