0e4b7382cc0cfb6674e664cb6c8f85867bce6273
“nidhiisinghh” <nidhiisinghh2007@gmail.com>
2025-04-15 12:24:40 +0530

    Updated code: [fingerprint biometric]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
 }
--- app/src/main/java/com/example/bookmydoc/view/MainActivity.kt
+++ app/src/main/java/com/example/bookmydoc/view/MainActivity.kt
@@ -1,21 +1,69 @@
 package com.example.bookmydoc.view
 
+import android.content.Intent
+import android.hardware.biometrics.BiometricManager
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.bookmydoc.R
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val loginFingerPrint: Button = findViewById(R.id.loginFingerPrintButton)
+
+        val biometricManager = androidx.biometric.BiometricManager.from(this)
+
+        when(biometricManager.canAuthenticate(androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG)){
+            BiometricManager.BIOMETRIC_SUCCESS-> {
+                loginFingerPrint.setOnClickListener {
+                    authenticateUser()
+                }
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE,
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE,
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE-> {
+                Toast.makeText(this, "Biometric not available", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun authenticateUser() {
+        val executor : Executor = ContextCompat.getMainExecutor(this)
+        val biometricPrompt = androidx.biometric.BiometricPrompt(this, executor, object : androidx.biometric.BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: androidx.biometric.BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Toast.makeText(applicationContext, "Fingerprint Matched", Toast.LENGTH_LONG).show()
+                startActivity(Intent(applicationContext, LoginActivity::class.java))
+                finish()
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Toast.makeText(applicationContext, "Fingerprint NOT Matched", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(applicationContext, "Clean Sensor/fatal error : $errString", Toast.LENGTH_LONG).show()
+            }
+        })
+
+        val promptInfo = androidx.biometric.BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric Authentication")
+            .setSubtitle("Use fingerprint to open app.")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
     }
 }
